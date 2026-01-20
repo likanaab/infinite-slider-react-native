f384517b9d8b179206cad8dee67fc994f225bf40
likanaab <lika.naab.4266@gmail.com>
2026-01-20 17:08:05 +0300

    new

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
-import { FlatList, StyleSheet, View, useWindowDimensions, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import  InfiniteSlider  from './components/Carousel';
-import { sliders } from './sliders';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 
 
 export default function App() {
   
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
       <View style={styles.container}>
         <InfiniteSlider />
     </View>
-    </GestureHandlerRootView>
   );
 };
 
@@ -22,23 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  sliderContainer:{
-    position:'relative',
-    justifyContent:'center',
-    alignItems:'center',
-    height:300,
-  },
-  imageWrapper:{
-    position:'absolute',
-    right:0,
-    left:0,
-    top:0,
-    bottom:0,
-    justifyContent:'center',
-    alignItems:'center',
-  },
-  viewBox:{
-    justifyContent:'center',
-    alignItems:'center',
-  }
 });
